d4fd28b92365edb8dceb5ca1ec251d566f589782
Nikita Volskii <nikitoriy@gmail.com>
2022-10-09 19:09:35 +0300

    chapter two completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const posts = [
+    {
+      id: 1,
+      text: 'Surprise, mafaka',
+      timestamp: 'A long, long time ago',
+      author: {
+        name: 'A good black person',
+      },
+    },
+    {
+      id: 2,
+      text: 'And I will strike down upon thee with great vengeance and furious anger!',
+      timestamp: 'A little less long ago',
+      author: {
+        name: 'Not so good black person',
+      },
+    },
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Microblog</h1>
+      {posts.length === 0?
+        <p>No posts to show</p>
+      :
+        posts.map(post => {
+          return (<p key = {post.id}>
+            <b>{post.author.name}</b> &mdash; {post.timestamp}
+            <br />
+            {post.text} 
+          </p>)
+        })}
+    </>
   );
 }
 
-export default App;
+
+export default App
